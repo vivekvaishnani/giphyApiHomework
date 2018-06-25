69442af9fb641ff6004b2fe751b9aaa3f92ba8b5
Vivek <vivekvaishnani89@gmail.com>
2018-06-25 13:52:08 -0400

    ok

--- docs/assets/api.js
+++ docs/assets/api.js
@@ -1,5 +1,28 @@
 
-var playerName =["Cristiano Ronaldo" , "Messi" , "David Beckhem"];
+var playerName = ["Cristiano Ronaldo", "Messi", "David Beckhem"];
+
+
+
+
+function renderButtons(){
+
+    // Delete the content inside the giphy-btn-view div prior to adding new animal buttons
+    // (this is necessary otherwise you will have repeat buttons)
+      $("#button-div").empty();
+    //Loop all in Animal array
+    for (var i = 0; i < playerName.length; i++) {
+        console.log(playerName[i]);
+        var btn = $("<button>");
+        btn.attr("player-name", playerName[i]);
+        btn.addClass("player");
+        btn.css("margin", "5px")
+        btn.addClass("btn btn-primary");
+        btn.text(playerName[i]);
+        $("#button-div").append(btn);
+    }
+}
+
+
 
 
 
@@ -7,87 +30,130 @@ function displayPlayerInfo() {
 
     $("#gif-display").empty();
 
-var sportsPlayers = $(this).attr("player-name");
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + sportsPlayers + "&api_key=hVsFi9ehA7uyv7PDoIqGVH4dQNESvsrj&limit=10";
+    var sportsPlayers = $(this).attr("player-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + sportsPlayers + "&api_key=hVsFi9ehA7uyv7PDoIqGVH4dQNESvsrj&limit=10";
 
 
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
 
-    for (i = 0; i < response.data.length; i++) {
-        
-        var imageUrl = response.data[i].images.original.url;
-        var imgInfo = JSON.stringify(response.data[i].slug);
-
-        // Creating and storing an image tag
-        var playerImage = $("<img>");
-        // Setting the Image src attribute to imageUrl
-        playerImage.attr("src", imageUrl);
-
-
-        var imgDiv = $("<div>");
-
-        imgDiv = playerImage;
-        
-        $(imgDiv).css('float:left');
-
-    
-
-        $("#gif-display").prepend(imgInfo,imgDiv,"<span>");
-
-        // Prepending the Image to the images div
-        // $("#gif-display").prepend("<span>", imgInfo, "<p>", playerImage, "<p>");
-
-
-    }
-
-});
-}
-
-
-
-
-
-function renderButton() {
-
-    $("#button-div").empty();
-    
-    for (var i = 0; i < playerName.length; i++) {
-
-        // Then dynamicaly generating buttons for each movie in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-        var a = $("<button>");
-        
-        a.addClass("player-btn");
-        a.addClass("btn-primary");
-        
-        // Adding a data-attribute
-        a.attr("player-name", playerName[i]);
-        // Providing the initial button text
-        a.text(playerName[i]);
+        console.log(response);
+        var promiseData = response.data;
        
-        $("#button-div").append("<span>",a);
-      }
+        dataDisplay(promiseData);
+
+
+    });
 }
 
 
+   function dataDisplay(source) {
+
+            var row1 = $("<div>");
+            row1.addClass("row");
+            row1.attr("id", "rowA");
+
+
+            var row2 = $("<div>");
+            row2.addClass("row");
+            row2.attr("id", "rowB");
+
+            var row3 = $("<div>");
+            row3.addClass("row");
+            row3.attr("id", "rowC");
+
+
+            $("#gif-display").append(row1);
+            $("#gif-display").append(row2);
+            $("#gif-display").append(row3);
+
+
+            for (var i = 0; i < source.length; i++) {
+                var playerDiv = $("<div>");
+                playerDiv.addClass("playerResult");
+                var img = $("<img>");
+                img.addClass("imgGif");
+                // img.addClass("col-lg-3");
+                
+
+                // ===========
+
+                if (i < 3) {
+                    playerDiv.addClass("row1");
+                    playerDiv.addClass("col-lg-3");
+                    $("#rowA").append(playerDiv);
 
 
 
-$("#add-player").on("click", function(event){
-event.preventDefault();
- 
-var sportsPlayers = $("#player-input").val().trim();
 
-playerName.push(sportsPlayers);
+                } else if (i >= 3 && i < 6) {
+                    playerDiv.addClass("row2");
+                    playerDiv.addClass("col-lg-3");
+                    $("#rowC").append(playerDiv);
 
-renderButton();
-});
+                }
+                else if (i >= 6 && i < 9) {
+                    playerDiv.addClass("row3");
+                    playerDiv.addClass("col-lg-3");
+                    $("#rowB").append(playerDiv);
 
-$(document).on("click" , ".player-btn" , displayPlayerInfo );
+                }
 
-renderButton();
+
+                // ===========
+
+                $(".row1")
+
+
+
+                var imgSrc = source[i].images.original.url;
+                var dataStill = source[i].images.fixed_height_still.url;
+                img.attr("data-still", dataStill);
+                console.log("Data Still added for image # " + i);
+                var dataPlayer = source[i].images.fixed_height.url;
+                img.attr("data-player", dataPlayer);
+                var dataState = "still";
+                img.attr("data-state", dataState);
+                img.attr("src", imgSrc);
+                var rating = ("Rating: " + source[i].rating);
+                var pOne = $("<p>");
+                pOne.text(rating)
+                playerDiv.prepend(img, pOne);
+                
+
+
+            }
+        }
+
+
+
+        $("#add-player").on("click", function(event) {
+           
+            event.preventDefault();
+    
+           
+            var sportsPlayer= $("#player-input").val().trim();
+            console.log("UserIP is "+sportsPlayer);
+           
+            playerName.push(sportsPlayer);
+            var abc = "";
+            
+            $("#player-input").val(abc);
+           
+            renderButtons();
+          });
+    
+   
+            renderButtons();
+    
+   
+    $(document).on("click", ".player", displayPlayerInfo);
+    
+    
+    // $(document).on("click", ".imgGif", pauseAndPlay);
+
+
+        
